7f7d935e4c915af1ba878cce6b59b3bd3ef49b33
nxxxshadowxxxn <nxxxshadowxxxn@gmail.com>
2025-09-27 12:35:40 +0000

    commit 29

--- js/config.js
+++ js/config.js
@@ -1,16 +1,15 @@
-
 // Archivo de configuración de Firebase
 // ¡¡¡ATENCIÓN!!!
 // REEMPLAZA los valores de marcador de posición con tus propias credenciales de Firebase.
 // NO compartas este archivo públicamente si contiene claves sensibles.
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "PEGA_TU_API_KEY_AQUI",
+  authDomain: "ecommerce-50cb5.firebaseapp.com",
+  projectId: "ecommerce-50cb5",
+  storageBucket: "PEGA_TU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "PEGA_TU_MESSAGING_SENDER_ID_AQUI",
+  appId: "PEGA_TU_APP_ID_AQUI"
 };
 
 // Hacemos la configuración accesible globalmente
