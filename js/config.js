1471ded036929c42d37fe7d03dcbd7b7641b71b1
nxxxshadowxxxn <nxxxshadowxxxn@gmail.com>
2025-09-27 12:41:50 +0000

    commit 30

--- js/config.js
+++ js/config.js
@@ -4,12 +4,12 @@
 // NO compartas este archivo públicamente si contiene claves sensibles.
 
 const firebaseConfig = {
-  apiKey: "PEGA_TU_API_KEY_AQUI",
+  apiKey: "AIzaSyA878OvPhYOz6tJqgh9YlhSIqI5NJxM3uc",
   authDomain: "ecommerce-50cb5.firebaseapp.com",
   projectId: "ecommerce-50cb5",
-  storageBucket: "PEGA_TU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "PEGA_TU_MESSAGING_SENDER_ID_AQUI",
-  appId: "PEGA_TU_APP_ID_AQUI"
+  storageBucket: "ecommerce-50cb5.firebasestorage.app",
+  messagingSenderId: "1046723710083",
+  appId: "1:1046723710083:web:f6618809a1b3c9bb30dfb6"
 };
 
 // Hacemos la configuración accesible globalmente
